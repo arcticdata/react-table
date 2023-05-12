5bdf57e4ae5fdf7e9677d11ed19349656f1f2fb7
wushaoqing <wushaoqing@datarc.cn>
2023-05-12 16:01:35 +0800

    refactor: 恢复组件文件结构，升级部分第三方库版本

---
+++ packages/demo/webpack.config.js
@@ -0,0 +1,56 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = (env = {}) => {
+  return {
+    entry: path.resolve(__dirname, 'src/main.tsx'),
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      libraryTarget: 'umd',
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: `index.html`,
+        template: path.resolve(__dirname, 'src/template.html'),
+      }),
+    ],
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+
+    stats: {
+      warningsFilter: /export .* was not found in/
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          loader: 'ts-loader',
+          options: { transpileOnly: true },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css?$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
+        },
+      ],
+    },
+
+    devServer: {
+      hot: true,
+      port: 8080,
+    },
+  }
+}
---
+++ packages/website/example-sidebars.js
@@ -0,0 +1,20 @@
+module.exports = {
+  sidebar: [
+    'index',
+    'table',
+    'pipeline',
+    'big-data',
+    'scroll-to',
+    'theme',
+    {
+      type: 'category',
+      label: '业务示例',
+      items: ['biz/data-report', 'biz/rank-table', 'biz/remain-matrix', 'biz/ROI-analysis-table'],
+    },
+    {
+      type: 'category',
+      label: '其他示例',
+      items: ['others/drill-tree-table', 'others/simple-pivot-table', 'others/cross-table-customization'],
+    },
+  ],
+}
